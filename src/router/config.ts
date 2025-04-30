832eb928a683e219159de1d1d0c46b4e6e2b3d46
thequeenshead <thequeensheadfarnham@gmail.com>
2025-04-30 16:36:07 +0100

    Minor Updates

--- src/router/config.ts
+++ src/router/config.ts
@@ -1,28 +1,28 @@
 const routes = [
-  {
-    path: ["/", "/home"],
-    exact: true,
-    component: "Home",
-  },
-  {
-    path: "/food",
-    exact: true,
-    component: "Food",
-  },
-  {
-    path: "/whatson",
-    exact: true,
-    component: "WhatsOn",
-  },
-  {
-    path: "/livemusic",
-    exact: true,
-    component: "LiveMusic",
-  },
-  {
-    path: "*",
-    component: "NotFound"
-  }
+	{
+		path: ["/", "/home"],
+		exact: true,
+		component: "Home",
+	},
+	{
+		path: "/food",
+		exact: true,
+		component: "Food",
+	},
+	{
+		path: "/whatson",
+		exact: true,
+		component: "WhatsOn",
+	},
+	{
+		path: "/livemusic",
+		exact: true,
+		component: "LiveMusic",
+	},
+	{
+		path: "*",
+		component: "NotFound"
+	}
 ];
 
 export default routes;
